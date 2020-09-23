e61b03ef44b54e76f5c479a36d233055f377b8cd
vagrant <vagrant@homestead>
2020-09-23 02:35:22 +0000

    laravel-bbs

---
+++ app/Http/Controllers/TopicsController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Link;
+use App\Models\Topic;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Requests\TopicRequest;
+use App\Models\Category;
+use Illuminate\Support\Facades\Auth;
+use App\Handlers\ImageUploadHandler;
+
+class TopicsController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
+
+	public function index(Topic $topic, Request $request,Link $link, User $user)
+	{
+		$topics = $topic->with('user', 'category')->withOrder($request->order)->paginate(10);
+        $links = $link->getAllCached();
+        $active_users = $user->getActiveUsers();
+		return view('topics.index', compact('topics','links', 'active_users'));
+	}
+
+    public function show(Topic $topic)
+    {
+        return view('topics.show', compact('topic'));
+    }
+
+	public function create(Topic $topic)
+	{
+		$categories = Category::all();
+        return view('topics.create_and_edit', compact('topic', 'categories'));
+	}
+
+	public function store(TopicRequest $request, Topic $topic)
+    {
+        $topic->fill($request->all());
+        $topic->user_id = Auth::id();
+        $topic->save();
+
+        return redirect()->route('topics.show', $topic->id)->with('success', '帖子创建成功！');
+    }
+
+	public function edit(Topic $topic)
+	{
+        $this->authorize('update', $topic);
+        $categories = Category::all();
+        return view('topics.create_and_edit', compact('topic', 'categories'));
+	}
+
+	public function update(TopicRequest $request, Topic $topic)
+	{
+		$this->authorize('update', $topic);
+		$topic->update($request->all());
+
+		return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
+	}
+
+	public function destroy(Topic $topic)
+	{
+		$this->authorize('destroy', $topic);
+		$topic->delete();
+
+		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
+	}
+
+
+    public function uploadImage(Request $request, ImageUploadHandler $uploader)
+    {
+        // 初始化返回数据，默认是失败的
+        $data = [
+            'success'   => false,
+            'msg'       => '上传失败!',
+            'file_path' => ''
+        ];
+        // 判断是否有上传文件，并赋值给 $file
+        if ($file = $request->upload_file) {
+            // 保存图片到本地
+            $result = $uploader->save($file, 'topics', \Auth::id(), 1024);
+            // 图片保存成功的话
+            if ($result) {
+                $data['file_path'] = $result['path'];
+                $data['msg']       = "上传成功!";
+                $data['success']   = true;
+            }
+        }
+        return $data;
+    }
+}
---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+            <span style="color: #e27575;font-size: 14px;">❤</span>
+            <a href="javascript: void(0);" target="_blank">WeiKai</a> 设计和编码
+            <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="float-right"><a href="mailto:827056678@qq.com">联系我们</a></p>
+    </div>
+</footer>
